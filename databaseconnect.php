e5d845fd495ceedc75433d3902ba5dcd7eee0a8a
maharjann162 <60078634+maharjann162@users.noreply.github.com>
2020-03-25 16:57:24 -0400

    I have created the connected file for the database and created the table for it

---
+++ databaseconnect.php
@@ -0,0 +1,8 @@
+//created the data base in mysql. 
+<?php
+$dbServerName="localhost";
+$dbusername="root";
+$dbpassword="";
+$dbName="tfits";
+$conn=mysqli_connect($dbServerName,$dbusername,$dbpassword,$dbName);
+?>
