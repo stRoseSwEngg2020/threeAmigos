c4af1c85ff9bf11f9de4b7750a667fe3dcde575e
cnolan1399 <cnolan1399@gmail.com>
2020-04-01 17:47:08 -0400

    result page

---
+++ result_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    -<title>Result Page</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php
+    echo "In $location, the temperature is about $temp"
+    echo "We reccomend you to wear $outfit."
+    ?>
+    <button>Accept</button>
+    <button>Decline</button>
+
+
+    </body>
+
+
+
+
+
+
+
+
+
+</html>
