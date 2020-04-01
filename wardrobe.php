00f78323fe35720160a45c43a548c7d408fb8749
maharjann162 <maharjann162@strose.edu>
2020-04-01 18:00:50 -0400

    updated changes - removed brand for now - php codes for inserting into database

--- wardrobe.php
+++ wardrobe.php
@@ -9,4 +9,10 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+$clothesType=$_POST["ctype"];
+$size=$_POST["size"];
+$activity=$_POST["act"];
+$season=$_POST["season"];
+$sql="INSERT INTO wardrobe(userId, clothesId, clothesType, size, activity, Season) VALUES (1,2,$clothesType,$size,$activity,$season);";
+mysqli_query($conn,$sql);
 ?>
