5371813212dbf2c23cfdd9534470828ec74efff2
jmontesanti <62080682+jmontesanti@users.noreply.github.com>
2020-04-02 18:15:38 -0400

    Add files via upload

---
+++ Location_form.php
@@ -0,0 +1,19 @@
+<html>
+<title> Location </title>
+  <body>
+    <form method="post" action="Location_form.php">
+      <br />
+      State:  <input type="text" size="20" name="state" />
+      <br />
+      City: <input type="text" size="20" name="city" />
+      <br />
+      Zipcode:  <input type="text" size="20" name="zipcode" />
+      <br />
+      Time: <input type="text" size="20" name="time" />
+      <br />
+      Activity: <input type="text" size="20" name="activity" />
+      <br />
+      <input type="submit" value="submit" name="submit" />
+    </form>
+  </body>
+</html>
