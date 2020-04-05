faf59af0cf378019d3f3b58bdea9010436deb825
maharjann162 <maharjann162@strose.edu>
2020-04-05 02:22:18 -0400

    changes die to database update and connected the php and html

--- Login_form.php
+++ Login_form.php
@@ -1,54 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
- p {
- text-align:center
-}
-</style>
 <meta charset="utf-8">
 <title>Login</title>
-<link rel="stylesheet" href="css/style.css" />
 </head>
-<style>
 <body>
 <?php
+//database connection added 
+$dbServerName="localhost";
+$dbusername="root";
+$dbpassword="";
+$dbName="tfits";
+$conn=mysqli_connect($dbServerName,$dbusername,$dbpassword,$dbName);
+
 // If form submitted, insert values into the database.
 if (isset($_POST['username'])){
         // removes backslashes
- $username = stripslashes($_REQUEST['username']);
+ $username = stripslashes($_POST['username']);
         //escapes special characters in a string
- $username = mysqli_real_escape_string($con,$username);
- $password = stripslashes($_REQUEST['password']);
- $password = mysqli_real_escape_string($con,$password);
+ $username = mysqli_real_escape_string($conn,$username);
+ $password = stripslashes($_POST['password']);
+ $password = mysqli_real_escape_string($conn,$password);
  //Checking is user existing in the database or not
-        $query = "SELECT * FROM `users` WHERE username='$username'
-and password='".md5($password)."'";
- $result = mysqli_query($con,$query) or die(mysql_error());
+        $query = "SELECT * FROM userinfo WHERE userName='$username'
+and password='".$password."'";
+ $result = mysqli_query($conn,$query) or die(mysql_error());
  $rows = mysqli_num_rows($result);
         if($rows==1){
      $_SESSION['username'] = $username;
-            // Redirect user to index.php
-     header("Location: index.php");
-         }else{
+     //need to chage this after the page is made
+     header("Location: wardrobe.html");
+         }
+       }
+       else{
  echo "<div class='form'>
 <h3>Username/password is incorrect.</h3>
 <br/>Click here to <a href='login.php'>Login</a></div>";
- }
-    }else{
+         }
 ?>
 </body>
 </html>
 
-<div class="form">
-<h1>Log In</h1>
-<form action="" method="post" name="login">
-<input type="text" name="username" placeholder="Username" required />
-<input type="password" name="password" placeholder="Password" required />
-<input name="submit" type="submit" value="Login" />
-</form>
-<p>Not registered yet? <a href='registration_form.php'>Register Here</a></p>
-</div>
-<?php } ?>
-</body>
-</html>
