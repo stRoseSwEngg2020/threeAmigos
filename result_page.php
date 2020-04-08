0075e3526c758a7768606346b6d47af1695f5494
jmontesanti <62080682+jmontesanti@users.noreply.github.com>
2020-04-08 11:24:21 -0400

    Styled The pages

--- result_page.php
+++ result_page.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    -<title>Result Page</title>
     <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>Result Page</title>
   </head>
   <body>
     <?php
